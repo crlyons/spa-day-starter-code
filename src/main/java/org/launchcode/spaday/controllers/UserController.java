987262818b8774b15d7d41300891a659a9079518
Carl <cindymadewhat@gmail.com>
2022-10-06 20:00:07 -0500

    studio 10.6

---
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -0,0 +1,25 @@
+package org.launchcode.spaday.controllers;
+
+import org.launchcode.spaday.models.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("user")
+public class UserController {
+    @RequestMapping("add")
+    public String displayAddUserForm(){
+        return "user/add";
+    }
+    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
+        model.addAttribute("user", user);
+        model.addAttribute("verify", verify);
+        model.addAttribute("username", user.getUsername());
+        model.addAttribute("email", user.getEmail());
+        return "";
+
+    }
+
+}
